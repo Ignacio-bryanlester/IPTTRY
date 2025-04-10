937a34024820421bf0da558e72787482e419181c
Ignacio-bryanlester <ignacio.brheu22@southernchristiancollege.edu.ph>
2025-04-10 11:21:21 +0800

    k

--- BlazorApp8/obj/Debug/net8.0/BlazorApp8.AssemblyInfo.cs
+++ BlazorApp8/obj/Debug/net8.0/BlazorApp8.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ede7815ada3feb892ae7e86314b10793a22dbe24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31aebfbca5c7ee932dc3b86a121755edb3ff950b")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ BlazorApp8/wwwroot/java.js
@@ -0,0 +1,20 @@
+﻿window.initializeSwiper = () => {
+    new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        loop: true,
+        autoplay: {
+            delay: 3500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        effect: "slide",
+    });
+};
